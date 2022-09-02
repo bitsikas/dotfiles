ed39155fa31bd971d4abc89ae327a73e11af1adc
Kostas Papakonstantinou <kostas@bitsikas.dev>
2022-09-02 17:45:49 +0300

    don't build git

--- git/git.nix
+++ git/git.nix
@@ -4,16 +4,6 @@
 
   programs.git = {
     enable = true;
-    package = pkgs.git.overrideAttrs(
-      old: {
-        version = "2.37.0";
-        src = pkgs.fetchurl {
-          url = "https://www.kernel.org/pub/software/scm/git/git-2.37.0.tar.xz";
-          sha256 = "9f7fa1711bd00c4ec3dde2fe44407dc13f12e4772b5e3c72a58db4c07495411f";
-        };
-
-      }
-      );
       userName = "Kostas Papakonstantinou";
       userEmail = "kostas@bitsikas.dev";
       extraConfig = {
