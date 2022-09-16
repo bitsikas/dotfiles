961c52994b82d2bd726f44ba76d9ec9cd49a3e67
Kostas Papakonstantinou <kostas@bitsikas.dev>
2022-09-16 16:59:54 +0300

    update configs to work on different envs

--- flake.nix
+++ flake.nix
@@ -10,43 +10,44 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, ... }: 
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let 
+    pkgs = import nixpkgs  { inherit system; } ;
+  in {
+    devShells.default = import ./shell.nix { inherit pkgs; };
+  }) // {
+    homeConfigurations = {
+      "kostas.papakon@PKOSTAS-MB" = home-manager.lib.homeManagerConfiguration {
+        configuration = import ./kostas.nix;
+        system = "x86_64-darwin";
+      };
+    };
 
-  {
-          # devShells.default = import ./shell.nix { inherit pkgs; };
-          # homeConfigurations = {
-          #   "kostas.papakon@PKOSTAS-MB" = home-manager.lib.homeManagerConfiguration {
-          #     configuration = import ./kostas.nix;
-          #     system = "x86_64-darwin";
-          #   };
-          # };
-
-          nixosConfigurations = {
-            spectre = nixpkgs.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [ 
-                ./configuration.nix 
-                ./hardware/spectre.nix 
-                home-manager.nixosModules.home-manager {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true; 
-                  home-manager.users.kostas = {
-                    home.username = "kostas";
-                    home.homeDirectory = "/home/kostas";
-                    home.stateVersion = "21.11";
-                    home.sessionVariables = {
-                      "EDITOR" = "nvim";
-                    };
-                    imports = [
-                      ./desktop.nix
-                      ./cli.nix
-                    ];
-                  };
+    nixosConfigurations = {
+      spectre = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ 
+          ./configuration.nix 
+          ./hardware/spectre.nix 
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true; 
+            home-manager.users.kostas = {
+              home.username = "kostas";
+              home.homeDirectory = "/home/kostas";
+              home.stateVersion = "21.11";
+              home.sessionVariables = {
+                "EDITOR" = "nvim";
+                };
+                imports = [
+                ./desktop.nix
+                ./cli.nix
+                ];
+                };
                 }
-              ];
+                ];
               # Example how to pass an arg to configuration.nix:
               # specialArgs = { hostname = "spectre"; };
-            }; 
-          };
-        };
-        }
+              }; 
+              };
+              };
+              } 
