15f48904a0e23fbc5a71cf0c79ebad1b73f912cf
Kostas Papakonstantinou <kostas@bitsikas.dev>
2023-09-11 09:46:34 +0300

    bump version

--- mac.nix
+++ mac.nix
@@ -12,6 +12,8 @@
   home.homeDirectory = "/Users/Kostas.Papakon";
   nixpkgs.config.permittedInsecurePackages = [
                 "nodejs-16.20.0"
+                "nodejs-16.20.2"
               ];
+
   programs.kitty.settings.text_composition_strategy = "4 30";
 }
