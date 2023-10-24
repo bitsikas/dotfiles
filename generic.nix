7b0c2d1babd2ce914c3862fb397a437815047fdb
Kostas Papakonstantinou <kostas@bitsikas.dev>
2023-10-24 17:07:11 +0300

    Add user

--- generic.nix
+++ generic.nix
@@ -4,6 +4,8 @@
   # nixpkgs.config.allowUnfree = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  home.username = "root";
+  home.homeDirectory = "/root/";
   home.stateVersion = "23.05";
   home.sessionVariables = {
     "EDITOR" = "nvim";
