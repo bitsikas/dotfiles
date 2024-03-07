f1ba436dc6645f338f3dfad3842701752af4196d
Kostas Papakonstantinou <kostas@bitsikas.dev>
2024-03-07 10:04:53 +0200

    use same name

--- generic.nix
+++ generic.nix
@@ -4,8 +4,8 @@
   # nixpkgs.config.allowUnfree = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  # home.username = "root";
-  # home.homeDirectory = "/root/";
+  home.username = "Kostas.Papakon";
+  home.homeDirectory = "/home/Kostas.Papakon";
   home.stateVersion = "23.05";
   home.sessionVariables = {
     "EDITOR" = "nvim";
